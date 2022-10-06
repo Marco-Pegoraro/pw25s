65ebcdaee2caebc26ecf15ba2595bc74e5664585
Marco Antonio Pegoraro <marco.antonio.pegoraro1@gmail.com>
2022-10-06 09:28:28 -0300

    alteracoes security e token

--- src/main/java/br/edu/utfpr/pb/pw25s/server/model/Register.java
+++ src/main/java/br/edu/utfpr/pb/pw25s/server/model/Register.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity(name = "tb_register")
@@ -27,6 +24,10 @@ public class Register {
 
     @NotNull
     private String bank;
+
+    @NotNull
+    @JoinColumn(name = "User_id")
+    private Long user;
 
     @NotNull
     private String account;
--- src/main/java/br/edu/utfpr/pb/pw25s/server/service/impl/UserServiceImpl.java
+++ src/main/java/br/edu/utfpr/pb/pw25s/server/service/impl/UserServiceImpl.java
@@ -3,19 +3,23 @@ package br.edu.utfpr.pb.pw25s.server.service.impl;
 import br.edu.utfpr.pb.pw25s.server.model.User;
 import br.edu.utfpr.pb.pw25s.server.repository.UserRepository;
 import br.edu.utfpr.pb.pw25s.server.service.UserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    BCryptPasswordEncoder passwordEncoder;
 
     public UserServiceImpl(UserRepository userRepository) {
+        passwordEncoder = new BCryptPasswordEncoder();
         this.userRepository = userRepository;
     }
 
     @Override
     public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
