547ba961d43db144cd77d9225492c8c68193c532
Marco Antonio Pegoraro <marco.antonio.pegoraro1@gmail.com>
2022-11-29 16:00:06 -0300

    modificacao annotations

--- src/main/java/br/edu/utfpr/pb/pw25s/server/model/User.java
+++ src/main/java/br/edu/utfpr/pb/pw25s/server/model/User.java
@@ -13,6 +13,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Collection;
@@ -33,6 +34,7 @@ public class User implements UserDetails {
     private String username;
 
     @NotNull
+    @NotEmpty
     private String email;
 
     @NotNull
